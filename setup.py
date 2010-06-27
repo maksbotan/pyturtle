8e2e271324a477a3387a3d779abbbbb5371caa4b
Maxim <kolmax94@gmail.com>
2010-06-27 23:09:17 +0400

    Added simple instalation script setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+__author__="maksbotan"
+__date__ ="$27.06.2010 22:47:41$"
+
+from distutils.core import setup
+
+setup (
+  name = 'Pyturtle',
+  version = '0.0.1',
+  description='Cross-toolkit Logo interpreter in Python',
+  py_modules=['pyturtle'],
+  packages=['pyturtle'],
+  requires=['gtk', 'glib', 'gobject', 'goocanvas', 'notify'],
+  url='http://github.com/maksbotan/pyturtle/',
+  author='maksbotan',
+  author_email='kolmax94@gmail.com',
+  license='GPL',
+)
